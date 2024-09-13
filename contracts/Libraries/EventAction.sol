5028c5573715578a26fad9edf047e584ae758706
Ezeozue Chiagoziem <ezeozuechiagoziem@gmail.com>
2024-09-13 17:33:15 +0100

    Update Counter After registration

--- contracts/Libraries/EventAction.sol
+++ contracts/Libraries/EventAction.sol
@@ -1,16 +1,13 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
+import { Error } from "./Errors.sol";
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import {Error} from "./Libraries/Errors.sol";
+library EventAction {
 
-contract EventManager {
     address NftAddress;
     uint256 public totalEvent;
 
-    constructor() {}
-
-    struct AttendantLog {
+     struct AttendantLog {
         address attendee;
         uint256 checkInTime;
     }
@@ -34,11 +31,8 @@ contract EventManager {
 
     mapping(uint256 => Event) events;
     mapping(address => mapping(uint256 => bool)) registeredUsers;
+
     
-
-    // For Manager
-
-
     function createEvent(
         address _NFTTokenAddress,
         string memory _name,
@@ -83,73 +77,13 @@ contract EventManager {
         ev.created_at = block.timestamp;
         ev.NFTTokenAddress = _NFTTokenAddress;
         ev.maxNumberOfRegisteration = _maxNumberOfRegistration;
+        totalEvent = eventId + 1;
         return ev;
-    }
-
-    function totalRegisteredEventMembers(uint eventId) external view returns (address [] memory) {
-        if (!isEventOwner(eventId)) {
-            revert Error.NotTheEventManager();
-        }
-
-        if (events[eventId].id < 1) {
-            revert Error.NotAValidEventId();
-        }
-        return events[eventId].attendees;
-    }
-
-    function registerForAnEvent(uint256 eventId) external {
-        if (events[eventId].id < 1) {
-            revert Error.NotAValidEventId();
-        }
-
-        if (registeredUsers[msg.sender][eventId]) {
-            revert Error.AlreadyRegistered();
-        }
-
-        if (events[eventId].isClosed) {
-            revert Error.EventIsClosed();
-        }
-
-        if (events[eventId].isCancelled) {
-            revert Error.EventIsCancelled();
-        }
-
-        address NFTAddr = events[eventId].NFTTokenAddress;
-        if (checkForNft(NFTAddr, msg.sender) < 1) {
-            revert Error.YouDontHaveOurNFT();
-        }
-        events[eventId].attendees.push(msg.sender);
-    }
-
-    function checkInForEvent(uint eventId, address _member) external returns(AttendantLog memory) {
-        
-        if (!isEventOwner(eventId)) {
-            revert Error.NotTheEventManager();
-        }
-        if (events[eventId].id < 1) {
-            revert Error.NotAValidEventId();
-        }
-        if (!registeredUsers[_member][eventId]) {
-            revert Error.NotRegisteredForEvent();
-        }
-        AttendantLog memory newLog = AttendantLog({
-            attendee: _member,
-            checkInTime: block.timestamp
-        });
-        events[eventId].attendantLog.push(newLog);
-        return newLog;
     }
 
     function _isEmptyString(string memory str) internal pure returns (bool) {
         return bytes(str).length == 0;
     }
 
-    function checkForNft(address _NFTContractAddress, address user) public view returns (uint) {
-        return IERC721(_NFTContractAddress).balanceOf(user);
-    }
-
-    function isEventOwner(uint256 _id) internal view returns (bool) {
-        return events[_id].created_by == msg.sender;
-    }
 
 }
